d58cbd56772ffb144b0aaf3d953dae95eac52b68
Agustin Molina <am.motos@hotmail.com>
2024-01-10 12:27:55 -0300

    Hero

--- app/components/HeroInfo.tsx
+++ app/components/HeroInfo.tsx
@@ -1,5 +1,7 @@
-import {Button} from '@nextui-org/react';
 import React from 'react';
+import {Button} from '@nextui-org/react';
+import {Link} from '@remix-run/react/dist/components';
+import {ChevronDoubleRightIcon} from '@heroicons/react/16/solid';
 
 const HeroInfo = () => {
   return (
@@ -18,8 +20,12 @@ const HeroInfo = () => {
       }}
     >
       <div>
-        <Button className="bg-white text-black px-4 mt-[38rem] rounded-lg shadow-lg">
-          Shop Now
+        <Button
+          href="/products"
+          className="bg-white text-black px-4 mt-[38rem] rounded-lg shadow-lg font-bold text-md"
+        >
+          Ver Coleccion
+          <ChevronDoubleRightIcon className="w-8 h-8 ml-2 animate-fade animate-infinite animate-duration-[1500ms]" />
         </Button>
       </div>
     </div>
