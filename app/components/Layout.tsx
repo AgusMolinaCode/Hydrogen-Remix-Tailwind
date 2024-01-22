4b6462941dc1c683a7a87586f0a3d87319586d9e
Agustin Molina <am.motos@hotmail.com>
2024-01-22 01:08:07 -0300

    Add SliderMenuFooter to Layout component

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -43,6 +43,8 @@ import {useIsHydrated} from '~/hooks/useIsHydrated';
 import {useCartFetchers} from '~/hooks/useCartFetchers';
 import {useRootLoaderData} from '~/root';
 
+import SliderMenuFooter from './SliderMenuFooter';
+
 type LayoutProps = {
   children: React.ReactNode;
   layout?: LayoutQuery & {
@@ -67,8 +69,8 @@ export function Layout({children, layout}: LayoutProps) {
         <main role="main" id="mainContent" className="">
           {children}
         </main>
+        {footerMenu && <Footer menu={footerMenu} />}
       </div>
-      {footerMenu && <Footer menu={footerMenu} />}
     </>
   );
 }
@@ -464,22 +466,10 @@ function Footer({menu}: {menu?: EnhancedMenu}) {
     : [];
 
   return (
-    <Section
-      divider={isHome ? 'none' : 'top'}
-      as="footer"
-      role="contentinfo"
-      className={`grid min-h-[25rem] items-start grid-flow-row w-full gap-6 py-8 px-6 md:px-8 lg:px-12 md:gap-8 lg:gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-${itemsCount}
-        bg-primary dark:bg-contrast dark:text-primary text-contrast overflow-hidden`}
-    >
+    <div className="pt-32">
       <FooterMenu menu={menu} />
-      <CountrySelector />
-      <div
-        className={`self-end pt-8 opacity-50 md:col-span-2 lg:col-span-${itemsCount}`}
-      >
-        &copy; {new Date().getFullYear()} / Shopify, Inc. Hydrogen is an MIT
-        Licensed Open Source project.
-      </div>
-    </Section>
+      <SliderMenuFooter />
+    </div>
   );
 }
 
---
+++ app/components/SliderMenuFooter.tsx
@@ -0,0 +1,81 @@
+import '../styles/app.css';
+import {useRef} from 'react';
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from 'framer-motion';
+import {wrap} from '@motionone/utils';
+
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+function ParallaxText({children, baseVelocity = 80}: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const {scrollY} = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 500], [0, 5], {
+    clamp: false,
+  });
+
+  /**
+   * This is a magic wrapping for the length of the text - you
+   * have to replace for wrapping that works for you or dynamically
+   * calculate
+   */
+  const x = useTransform(baseX, (v) => `${wrap(-0, -200, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    /**
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
+     */
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
+  return (
+    <div className="parallax">
+      <motion.div className="scroller w-full" style={{x}}>
+        {[...Array(30)].map((_, i) => (
+          <p
+            key={i}
+            className="text-[10rem] sm:text-[18rem] tracking-normal flex items-center justify-center  font-racing bg-gray-600/45 bg-clip-text text-transparent pr-2"
+          >
+            {children}{' '}
+          </p>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
+
+export default function SliderMenuFooter() {
+  return (
+    <section>
+      <ParallaxText baseVelocity={3}>AM MOTOS .</ParallaxText>
+    </section>
+  );
+}
